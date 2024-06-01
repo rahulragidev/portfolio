864976ec968c35d0a74e710509c107fae10b6db0
rahulragidev <iamrahulragi@gmail.com>
2024-06-01 23:04:42 +0530

    basic profolio setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="relative bg-black">
-			<div>
-				<h1>Welcome to my portfolio!</h1>
-				<p>
-					I am Rahul Ragi, a passionate Full Stack Developer with a robust
-					background in creating dynamic and responsive web applications. With a
-					keen interest in modern technologies and a commitment to continuous
-					learning, I thrive on building innovative solutions that enhance user
-					experiences and drive business growth.
-				</p>
+		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+			<div className="max-w-7xl w-full">
+				<Hero />
 			</div>
 		</main>
 	);
