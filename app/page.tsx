680badaea3f57ae2c0ffd3bca710f31023e6b340
rahulragidev <iamrahulragi@gmail.com>
2024-06-08 06:23:02 +0530

    about-page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+"use client";
+
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
-export default function Home() {
-	return (
-		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-			<div className="max-w-7xl w-full">
-				<FloatingNav navItems={[
-					{name: 'Home', link: '/', icon: <FaHome />}
-				]}/>
-				<Hero />
-			</div>
-		</main>
-	);
-}
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,5 @@
 import { gridItems } from "@/data";
-import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 
 const Grid = () => {
   return (
